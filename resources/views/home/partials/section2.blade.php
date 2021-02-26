9a67e13ac01dad00f5a01483ee170caafac3686d
Afrasiyab Haider <afrasiyab555@gmail.com>
2021-02-26 17:15:40 +0500

    Update section2.blade.php

--- resources/views/home/partials/section2.blade.php
+++ resources/views/home/partials/section2.blade.php
@@ -12,17 +12,17 @@
             <div class="col-md-12">
                 <div class="service-slider">
                     <div class="service-block yellow">
-                        <img src="{{asset('images/service-icon2.png')}}" alt="#">
+                        <img src="{{asset('images/service-icon3.png')}}" alt="#">
                         <h3>Gynaecology</h3>
                         <p class="text-justify">
                              Gynaecology provides our patients with assorted selections for personal obstetric and gynaecologic care while offering a simple-access method of complete healthcare for ladies.
                         </p>
                         <a href="{{url('shahida-husain-tarar/')}}" class="btn btn-dark">READ MORE</a>
                         <div class="service-bg-icon">
-                            <img src="{{asset('images/services-bg1.png')}}" class="img-fluid" alt="#">
+                            <img src="{{asset('images/services-bg3.png')}}" class="img-fluid" alt="#">
                         </div>
                     </div>
-                    <div class="service-block green">
+                    <div class="service-block blue">
                         <img src="{{asset('images/service-icon3.png')}}" alt="#" />
                         <h3>Gastroenterology</h3>
                         <p class="text-justify">
@@ -30,7 +30,7 @@
                         </p>
                         <a href="{{url('muhammad-afzal-ch/')}}" class="btn btn-dark">READ MORE</a>
                         <div class="service-bg-icon">
-                            <img src="{{asset('images/services-bg2.png')}}" class="img-fluid" alt="#">
+                            <img src="{{asset('images/services-bg3.png')}}" class="img-fluid" alt="#">
                         </div>
                     </div>
                     <div class="service-block blue">
